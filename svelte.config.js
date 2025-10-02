41d4a145757e29077d2090467eb0042845550465
pwouik <pwouick@gmail.com>
2025-10-02 02:39:48 +0200

    idk

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,6 @@ const config = {
 	preprocess: [vitePreprocess(),mdsvex({extensions: [".svx", ".md"]})],
 
 	kit: {
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
